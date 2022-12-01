86ea2e8f2ff18d2cac1715c2b244d766e92f6494
Riicky-hub <richardjosephace1960@gmail.com>
2022-12-01 18:36:05 -0300

    finalizada projects header

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,16 +1,17 @@
-// Text
-const navLinks = document.querySelectorAll('li.nav-item > a.link');
-const navLinksArr = Array.from(navLinks);
-const showcaseTitle = document.querySelector('.showcase__title');
-const showcaseBottomParagraph = document.querySelector('.showcase__bottom--paragraph');
 // Options
 const selectedFlag = document.querySelector('.selected-lang > .flag_icon');
 const allFlags = document.querySelectorAll('[wn-lang]');
 const brazilFlag = document.querySelector('[wn-lang=portuguese]');
 const unitedKingdomFlag = document.querySelector('[wn-lang=english]');
+// Text
+const navLinks = document.querySelectorAll('li.nav-item > a.link');
+const navLinksArr = Array.from(navLinks);
+const showcaseTitle = document.querySelector('.showcase__title');
+const showcaseBottomParagraph = document.querySelector('.showcase__bottom--paragraph');
 
-
-
+const projectsHeaderInfoTitle = document.querySelector('.projects__header--info > h3');
+const projectsHeaderInfoParagraph = document.querySelector('.projects__header--info > p');
+const eclipseLink = document.querySelector('.eclipse__link');
 
 /* <-------English-------> */
 const navEnglish = ['Home', 'Projects', 'My skills', 'About me', 'Contact'];
@@ -23,6 +24,9 @@ unitedKingdomFlag.addEventListener('click', (event) => {
     });
     showcaseTitle.innerHTML = `Welcome to my professional portfolio as a web developer<span class="span--color">.</span>`;
     showcaseBottomParagraph.innerHTML = `With broad knowledge in various technologies and in various aspects, such as front-end and back-end, I can transform your ideas into business`;
+    projectsHeaderInfoTitle.innerHTML = `The codes of my projects<br> are all available on GitHub`;
+    projectsHeaderInfoParagraph.innerHTML = `My work as a programmer I take as an art, always paying attention to details.`;
+    eclipseLink.innerHTML = `View all projects <img src="./src/images/draws/Eclipse(blackAndWhite)/Arrow.svg" class="eclipse__arrow">`;
     allFlags.forEach(e => e.classList.remove('active'));
     unitedKingdomFlag.classList.add('active');
     localStorage.setItem('language', 'EN-GB');
@@ -36,6 +40,9 @@ function changeToEnglish() {
     });
     showcaseTitle.innerHTML = `Welcome to my professional portfolio as a web developer<span class="span--color">.</span>`;
     showcaseBottomParagraph.innerHTML = `With broad knowledge in various technologies and in various aspects, such as front-end and back-end, I can transform your ideas into business`;
+    projectsHeaderInfoTitle.innerHTML = `The codes of my projects<br> are all available on GitHub`;
+    projectsHeaderInfoParagraph.innerHTML = `My work as a programmer I take as an art, always paying attention to details.`;
+    eclipseLink.innerHTML = `View all projects <img src="./src/images/draws/Eclipse(blackAndWhite)/Arrow.svg" class="eclipse__arrow">`;
     allFlags.forEach(e => e.classList.remove('active'));
     unitedKingdomFlag.classList.add('active');
 }
@@ -56,6 +63,9 @@ brazilFlag.addEventListener('click', (event) => {
     });
     showcaseTitle.innerHTML = `Bem-vindo ao meu portfolio<br> profissional como programador web<span class="span--color">.</span>`;
     showcaseBottomParagraph.innerHTML = `Com conhecimento amplo em várias tecnologias<br> e em várias vertentes, como front-end e back-end<br> transformo suas ideias em négocios`;
+    projectsHeaderInfoTitle.innerHTML = `Os códigos dos meus projetos<br> estão todos no GitHub`;
+    projectsHeaderInfoParagraph.innerHTML = `Meu trabalho como programador eu levo como uma arte, sempre dando atenção aos detalhes.`;
+    eclipseLink.innerHTML = `Ver todos os projetos <img src="./src/images/draws/Eclipse(blackAndWhite)/Arrow.svg" class="eclipse__arrow">`;
     allFlags.forEach(e => e.classList.remove('active'));
     brazilFlag.classList.add('active');
     localStorage.setItem('language', 'PT-BR');
@@ -68,6 +78,9 @@ function changeToPortuguese() {
     });
     showcaseTitle.innerHTML = `Bem-vindo ao meu portfolio<br> profissional como programador web<span class="span--color">.</span>`;
     showcaseBottomParagraph.innerHTML = `Com conhecimento amplo em várias tecnologias<br> e em várias vertentes, como front-end e back-end<br> transformo suas ideias em négocios`;
+    projectsHeaderInfoTitle.innerHTML = `Os códigos dos meus projetos<br> estão todos no GitHub`;
+    projectsHeaderInfoParagraph.innerHTML = `Meu trabalho como programador eu levo como uma arte, sempre dando atenção aos detalhes.`;
+    eclipseLink.innerHTML = `Ver todos os projetos <img src="./src/images/draws/Eclipse(blackAndWhite)/Arrow.svg" class="eclipse__arrow">`;
     allFlags.forEach(e => e.classList.remove('active'));
     brazilFlag.classList.add('active');
 }
@@ -75,3 +88,6 @@ function changeToPortuguese() {
 if(localStorage.language === 'PT-BR') {
     changeToPortuguese();
 }
+
+// AOS
+AOS.init();
